be4975f244da9b7400dbd75944cedd49fcd13a2b
liuzm <1356553590@qq.com>
2017-05-03 09:57:32 +0800

    theme beauty customize

---
+++ theme_beauty_customize/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Theme Beauty Customize',
+    'version': '0.1',
+    'description': """
+Theme Beauty Customize
+""",
+    'author': 'oducn.com',
+    'website': 'http://oducn.com',
+    'depends': [
+        'website',
+        'theme_loftspace',
+    ],
+    'installable': True,
+    'application': True,
+    'data': [
+        'views/theme.xml',
+        'views/customize_loftspace.xml',
+    ],
+}
