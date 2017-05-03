fe83c5f8eda6d75f3b935bcc9c34715199ee3205
liuzm <1356553590@qq.com>
2017-05-03 10:26:39 +0800

    sale step

---
+++ website_sale_customize/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Website Sale Customize',
+    'summary': "Add Mobile's Judgement for website_sale.confirmation",
+    'category': 'Website Sale',
+    'version': '1.0',
+    'description': """
+Add Mobile's Judgement
+    """,
+    'author': 'Liuzm',
+    'website': 'http://oducn.com',
+    'depends': ['website_sale','website_mobile_menu'],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
