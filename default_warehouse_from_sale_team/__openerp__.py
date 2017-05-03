6630c3a8dc839816153c094588e597c93ae6349f
liuzm <1356553590@qq.com>
2017-05-03 09:49:55 +0800

    base model for rma

---
+++ default_warehouse_from_sale_team/__openerp__.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+{
+    'name': "Sale Team Warehouse",
+    'summary': """Adding a field for the default user warehouse and modifying
+    the global default method for assign in any related field the correct
+    default warehouse.
+    """,
+    'author': "Vauxoo",
+    'website': "http://www.vauxoo.com",
+    'license': 'AGPL-3',
+    'category': '',
+    'version': '8.0.2.0.0',
+    'depends': [
+        'sale_stock',
+        #  TODO: Remove "delivery" if the following issue is fixed
+        #  https://github.com/odoo/odoo/pull/13458
+        'delivery',
+        'sales_team',
+        'base_action_rule',
+        'purchase_requisition',
+    ],
+    'test': [
+    ],
+    'data': [
+        'views/sales_team_view.xml',
+        'views/res_users_view.xml',
+        'views/stock_view.xml',
+        'security/ir.model.access.csv',
+        'security/res_groups.xml',
+        'security/ir_rule.xml',
+        'data/ir_actions_server.xml',
+        'data/base_action_rule.xml',
+    ],
+    'demo': [
+        'demo/stock_demo.xml',
+    ],
+}
---
+++ default_warehouse_from_sale_team/models/sales_team.py
@@ -0,0 +1,138 @@
+# coding: utf-8
+
+from openerp import SUPERUSER_ID, api, fields, models
+
+
+class InheritedCrmSaseSection(models.Model):
+
+    _inherit = "crm.case.section"
+
+    default_warehouse = fields.Many2one('stock.warehouse',
+                                        string='Default Warehouse',
+                                        help='In this field can be '
+                                        'defined a default warehouse for '
+                                        'the related users to the sales team.')
+    journal_team_ids = fields.One2many(
+        'account.journal', 'section_id', string="Journal's sales teams",
+        help="Choose the Journals that user with this sale team can see")
+    journal_stock_id = fields.Many2one(
+        'account.journal', 'Journal stock valuation',
+        help='It indicates journal to be used when move line is created with'
+        'the warehouse of this sale team')
+
+    @api.multi
+    def update_users_sales_teams(self):
+        """ This method will review the users every time a sale team is create
+        / write to:
+
+        - if add a member and this one has not default sale team then set the
+          current team as the new default sale team
+        - if remove a member of the sale team and this one has as default the
+          current sale team then will update the user to set default sale teams
+          to False
+        - if they are just added or remove from the sale team we need to make
+          a dummy write in order to update the user sales_team_wh_ids and this
+          way the filtering rule will works to show the records only related to
+          the real user current sale teams configured warehouses.
+        """
+        for team in self:
+
+            # Add default team to users without default
+            wo_default_team = team.member_ids.filtered(
+                lambda user: not user.default_section_id)
+            for user in wo_default_team:
+                user.write({'default_section_id': team.id})
+
+            # Remove default team for users that are not longer in the current
+            # team
+            default_current_team = self.env['res.users'].search(
+                [('default_section_id', '=', team.id)])
+            remove_default_team = default_current_team - team.member_ids
+            for user in remove_default_team:
+                user.write({'default_section_id': False})
+
+            # Dummy write to update the m2m user.sale_teams in order to be
+            # capable of rendering the ir.rules properly.
+            for member in team.member_ids:
+                member.write({})
+
+        return True
+
+
+class WarehouseDefault(models.Model):
+    """If you inherit from this model and add a field called warehouse_id into
+    the model itself then the default value for such model will be the one
+    setted into the sales team.
+    """
+
+    _name = "default.warehouse"
+
+    @api.model
+    def default_get(self, fields_list):
+        """Force that if model has a field called warehouse_id the default
+        value is the one in the sales team in the user setted
+        """
+        defaults = super(WarehouseDefault,
+                         self).default_get(fields_list)
+        res_users_obj = self.env['res.users']
+        user_brw = res_users_obj.browse(self._uid)
+        warehouse = user_brw.default_section_id.default_warehouse
+        if warehouse:
+            warehouse_id = warehouse.id
+            model_obj = self.env['ir.model']
+            fields_obj = self.env['ir.model.fields']
+            model_id = model_obj.search([('model', '=', self._model._name)])
+            fields_data = fields_obj.search(
+                [('model_id', '=', model_id.id),
+                 ('relation', '=', 'stock.warehouse'),
+                 ('ttype', '=', 'many2one')])
+            names_list = list(set([field.name for field in fields_data]))
+            defaults.update(
+                {name: warehouse_id for name in names_list
+                 if defaults.get(name)})
+        return defaults
+
+    @api.v7
+    def read(self, cr, user, ids, fields_list=None, context=None,
+             load='_classic_read'):
+        """This method is overwrite because we need to propagate SUPERUSER_ID
+        when picking are chained in another warehouse without access to read"""
+        if self.pool.get('res.users').has_group(
+            cr, user, 'default_warehouse_from_sale_team.'
+                'group_limited_default_warehouse_sp'):
+            # we need to change to SUPERUSER_ID to allow access to read
+            user = SUPERUSER_ID
+        return super(WarehouseDefault, self).read(
+            cr, user, ids, fields=fields_list, context=context, load=load)
+
+    # pylint: disable=function-redefined
+    # this comment is for avoid error to travis by different apis
+    @api.v8
+    def read(self, fields_list=None, load='_classic_read'):
+        """This method is overwrite because we need to propagate SUPERUSER_ID
+        when picking are chained in another warehouse without access to read"""
+        if self.env.user.has_group('default_warehouse_from_sale_team.'
+                                   'group_limited_default_warehouse_sp'):
+            # we need to change to SUPERUSER_ID to allow access to read
+            self = self.sudo()
+        return super(WarehouseDefault, self).read(fields_list, load)
+
+    @api.model
+    def create(self, vals):
+        sequence_obj = self.env['ir.sequence']
+        pick_type_obj = self.env['stock.picking.type']
+        if vals.get('warehouse_id', 'picking_type_id'):
+            code = self._name
+            if code == 'purchase.requisition':
+                code = 'purchase.order.requisition'
+            pick_warehouse_id = pick_type_obj.browse(
+                vals.get('picking_type_id')).warehouse_id.id
+            warehouse_id = vals.get('warehouse_id', pick_warehouse_id)
+            section_id = self.env['crm.case.section'].search(
+                [('default_warehouse', '=', warehouse_id)], limit=1)
+            sequence_id = sequence_obj.search(
+                [('section_id', '=', section_id.id),
+                 ('code', '=', code)], limit=1)
+            if sequence_id:
+                vals['name'] = sequence_obj.get_id(sequence_id.id, 'id')
+        return super(WarehouseDefault, self).create(vals)
