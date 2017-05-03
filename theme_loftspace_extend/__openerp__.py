0fb006cfe8ccd122a9fb1b98a76df5352c6c42df
liuzm <1356553590@qq.com>
2017-05-03 10:02:53 +0800

    translate and tour

---
+++ theme_loftspace_extend/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Loftspace Theme Extend',
+    'description': """
+    添加插件中文翻译\n
+    添加主题配送方案
+    """,
+    'category': 'Theme/Ecommerce',
+    'version': '1.0',
+    'author': 'oducn.com Liuzm',
+    'depends': ['theme_loftspace'],
+    'data': [
+        'views/snippets.xml',
+        'views/customize_modal.xml',
+    ],
+    'demo': [
+    ],
+    'images': [
+    ],
+}
